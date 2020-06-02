475580fa71b9969dc596a6a8f4b26c7525d7d045
Tasha Arvanitis <tasha.arvanitis@gmail.com>
2020-06-02 10:45:48 -0500

    Create console app and do fizzbuzz

---
+++ consoleApp/consoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Counting...");
+            for (int i = 0; i<30; i++)
+            {
+                Console.Write(count + " ");
+                count += 1;
+                if (i % 3 == 0) {
+                    Console.Write("fizz");
+                }
+                if (i % 5 == 0) {
+                    Console.Write("buzz");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
